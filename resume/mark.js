a0d12cda45e0c7c24783ac9cf661ae115920edc2
Mark Beeson <spam-github@encodedrecords.com>
2013-01-08 13:39:30 -0800

    CSS transform and JS fix.

--- resume/mark.js
+++ resume/mark.js
@@ -1,18 +1,21 @@
 ;$(function() {
 
 	showTweets = function($div, word, f) {
+		var twoproblems = new RegExp(word, "i")
 		$.getJSON("http://search.twitter.com/search.json?rpp=10&q=" + encodeURIComponent(word) + "&result_type=recent&geocode=37.781157,-122.398720,25mi&callback=?", function(data) {
 			var results, i, tweet, html, text, from_user_name;
 			results = data.results;
 			for(i=0;i<results.length;i++) {
 				tweet = results[i];
 				text = tweet.text;
-				text = text.replace(new RegExp(word, "i"), "<em>" + word + "</em>");
-				from_user_name = tweet.from_user_name.replace(new RegExp(word, "i"), "<em>" + word + "</em>");
-				html = "<div class=\"item " + (i == 0 ? "active" : "") + "\"><span class=\"profile_image_url\"><img class=\"profile\" src=\"" + tweet.profile_image_url + "\" width=\"48\" height=\"48\"></span>" +
-					"<span class=\"from_user_name\">" + from_user_name + " says: </span>" +
-					"<span class=\"text\">" + text + "</span></div>";
-				$div.append(html);
+				if(twoproblems.test(text)) {
+					text = text.replace(twoproblems, "<em>" + word + "</em>");
+					from_user_name = tweet.from_user_name.replace(new RegExp(word, "i"), "<em>" + word + "</em>");
+					html = "<div class=\"item " + (i == 0 ? "active" : "") + "\"><span class=\"profile_image_url\"><img class=\"profile\" src=\"" + tweet.profile_image_url + "\" width=\"48\" height=\"48\"></span>" +
+						"<span class=\"from_user_name\">" + from_user_name + " says: </span>" +
+						"<span class=\"text\">" + text + "</span></div>";
+						$div.append(html);
+				}
 			}
 			f();
 		});
@@ -34,5 +37,7 @@
 		email = "mark.resume." + (d.getYear() + 1900) + (d.getMonth() + 1) + d.getDate() + d.getHours() + d.getMinutes() + "@markbeeson.net";
 		$this.html("<a href=\"mailto:" + email + "\">" + email + "</a>");
 	});
+	
+	$("body").addClass("loaded");
 	
 });
