75f122ef6cbfc73066adbc0fb3fe716b61c6bd14
Mark Beeson <spam-github@encodedrecords.com>
2013-01-09 12:23:08 -0800

    Bugfix for first tweet not matching

--- resume/mark.js
+++ resume/mark.js
@@ -3,18 +3,20 @@
 	showTweets = function($div, word, f) {
 		var twoproblems = new RegExp(word, "i")
 		$.getJSON("http://search.twitter.com/search.json?rpp=10&q=" + encodeURIComponent(word) + "&result_type=recent&geocode=37.781157,-122.398720,25mi&callback=?", function(data) {
-			var results, i, tweet, html, text, from_user_name;
+			var results, i, first, tweet, html, text, from_user_name;
 			results = data.results;
+			first = true
 			for(i=0;i<results.length;i++) {
 				tweet = results[i];
 				text = tweet.text;
 				if(twoproblems.test(text)) {
 					text = text.replace(twoproblems, "<em>" + word + "</em>");
 					from_user_name = tweet.from_user_name.replace(new RegExp(word, "i"), "<em>" + word + "</em>");
-					html = "<div class=\"item " + (i == 0 ? "active" : "") + "\"><span class=\"profile_image_url\"><img class=\"profile\" src=\"" + tweet.profile_image_url + "\" width=\"48\" height=\"48\"></span>" +
+					html = "<div class=\"item " + (first ? "active" : "") + "\"><span class=\"profile_image_url\"><img class=\"profile\" src=\"" + tweet.profile_image_url + "\" width=\"48\" height=\"48\"></span>" +
 						"<span class=\"from_user_name\">" + from_user_name + " says: </span>" +
 						"<span class=\"text\">" + text + "</span></div>";
 						$div.append(html);
+					first = false
 				}
 			}
 			f();
